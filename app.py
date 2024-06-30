2898521ae95f9184856676647cacc696dde574c1
pnumin <pnumin@pusan.ac.kr>
2024-06-30 22:48:30 +0900

    데이터프레임

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd 
 
-st.write('Hello, *World!* :sunglasses:')
-st.write('Hello, Stremlit!!')
+st.write('# 설문분석')
 
 # 사이드바에 파일 업로드 기능 추가
 uploaded_file = st.sidebar.file_uploader("파일 업로드", type=["csv"])
@@ -12,11 +11,9 @@ if uploaded_file is not None:
     data = pd.read_csv(uploaded_file, encoding='cp949')
 
     st.write("업로드한 데이터:")
-    st.write(data)
 
-    # 데이터프레임 정보 표시
-    st.write("데이터프레임 정보:")
-    st.write(data.describe())
+    df = data.iloc[:, 1:]
+    st.write(df) 
  
 else:
     st.write("Excel 파일을 업로드해주세요.")
