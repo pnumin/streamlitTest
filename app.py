682e7a831b418c2602a58f6b50ee80bc0b70362c
pnumin <pnumin@pusan.ac.kr>
2024-06-30 22:56:41 +0900

    수정

--- app.py
+++ app.py
@@ -6,14 +6,11 @@ st.write('# 설문분석')
 # 사이드바에 파일 업로드 기능 추가
 uploaded_file = st.sidebar.file_uploader("파일 업로드", type=["csv"])
 
-if uploaded_file is not None:
+if uploaded_file is None:
+    st.write("Excel 파일을 업로드해주세요.")
+else :    
     # csv 파일 읽기
     data = pd.read_csv(uploaded_file, encoding='cp949')
 
-    st.write("업로드한 데이터:")
-
-    df = data.iloc[:, 1:]
-    st.write(df) 
- 
-else:
-    st.write("Excel 파일을 업로드해주세요.")
+    st.write("업로드한 데이터:") 
+    st.write(data)
