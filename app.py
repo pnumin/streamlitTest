d00863197cfd2b7b76695859a790d0354f6569d9
pnumin <pnumin@pusan.ac.kr>
2024-07-04 23:25:06 +0900

    메뉴만들기

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
 import streamlit as st
+import dice
+import updown
+import df
 
-st.write('Hello, *World!* :sunglasses:') 
+st.sidebar.title("메뉴선택")
+page = st.sidebar.radio("", ["주사위던지기", "업다운게임", "데이터분석"])
+
+if page == "주사위던지기":
+  st.title("주사위던지기")
+  dice.show()
+
+elif page == "업다운게임":
+  st.title("업다운게임")
+  updown.show()
+
+elif page == "데이터분석":
+  st.title("데이터분석")
+  df.show()
+
+  
